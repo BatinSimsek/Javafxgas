9f49b9a8bd3fd9ae82e4f312e69d63566cb8cb99
BSIMSEK <batinsimsek1998@gmail.com>
2022-11-15 11:14:48 +0100

    FX
    JAva

--- gas/src/main/java/gui/gas/Customer.java
+++ gas/src/main/java/gui/gas/Customer.java
@@ -1,22 +1,44 @@
 package gui.gas;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-
-
 
 public class Customer {
 
     public Scene getView(){
-        StackPane layout = new StackPane();
-        Label texts = new Label("a");
+        GridPane layout = new GridPane();
 
-        layout.getChildren().add(texts);
+        Label custommerNumber = new Label("Klantnummer:");
+        TextField customerNumberText = new TextField();
+        Label firstName = new Label("Voornaam:");
+        TextField firstNameText = new TextField();
+        Label sureName = new Label("Achternaam:");
+        TextField sureNameText = new TextField();
+        Label yearPrice = new Label("Jaarlijks voorschot:");
+        TextField yearPriceText = new TextField();
+        Button buttonSave = new Button("Opslaan");
 
-        Scene scene = new Scene(layout);
+        //0-0
+        layout.add(customerNumberText, 1, 0);
+        layout.add(custommerNumber, 0, 0);
+        //0-1
+        layout.add(firstName, 0, 1);
+        layout.add(firstNameText, 1 , 1);
+        //0-2
+
+
+
+
+
+        layout.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(layout, 500 , 500);
         return scene;
     }
 
